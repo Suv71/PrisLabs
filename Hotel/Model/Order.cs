42520bb57d72c13a0cd717a55f0884cee8e26ada
DmitriyMikhalin <d.mihalin@yandex.ru>
2018-09-29 11:06:22 +0300

    Добавлены модель, уровень доступа к данным с интерфейсами репозиториев, проект бизнес-логики

---
+++ Hotel/Model/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid RoomId { get; set; }
+        public DateTime ArrivedDate { get; set; }
+        public DateTime LeavedDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public Order()
+        {
+        }
+    }
+}
---
+++ Hotel/Model/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public int Capacity { get; set; }
+        public Guid RoomTypeId { get; set; }
+        public double Cost { get; set; }
+
+        public Room()
+        {
+        }
+    }
+}
